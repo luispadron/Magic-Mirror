793a21dc66cb09392f6e7b9544ee87e5b0660d64
Luis Padron <luispadronedu@gmail.com>
2016-04-19 19:14:37 -0400

    add full api url getter

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
-var forecastAPI = 'https://api.forecast.io/forecast//37.8267,-122.423';
+var forecastURL = '';
 
-function getApiKeyFromFile() {
+// Grabs API Key from file, so it's not on github
+function getForecastURL() {
   $.get('assets/apikey.txt', function(data) {
-      alert(data);
+    // Set appropriate latitude and longitude for location here
+    var latitude = '28.521824';
+    var longitude = '-81.224258';
+    forecastURL += 'https://api.forecast.io/forecast/' + data.trim() + '/' + latitude + ',' + longitude;
+    console.log(forecastURL);
   });
 }
 
-getApiKeyFromFile();
+getForecastURL();
