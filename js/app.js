df8ee59a4b6f4b323c0e6831ff41f746a9bbf17b
Luis Padron <luispadronedu@gmail.com>
2016-04-19 20:03:16 -0400

    add getting of weather and changing temp label

--- js/app.js
+++ js/app.js
@@ -1,4 +1,28 @@
+// Globals
 var forecastURL = '';
+
+// Updates the weather panel with the retrieved
+// Weather information
+function updateWeatherInfo(data) {
+  console.log('Updating weather info!');
+  // Round temp down
+  var temp = Math.floor(data.currently.temperature);
+  // Set temp
+  $('#weather-info span').text(temp + '°');
+}
+
+// Makes a JSONP request to developer.forecast.io,
+// Callback on success: updateWeatherInfo(data)
+function requestForecastData() {
+    $.ajax({
+      type: 'GET',
+      dataType: 'jsonp',
+      url: forecastURL,
+      success: function(data) {
+        updateWeatherInfo(data);
+      }
+    });
+}
 
 // Grabs API Key from file, so it's not on github
 function getForecastURL() {
@@ -8,6 +32,7 @@ function getForecastURL() {
     var longitude = '-81.224258';
     forecastURL += 'https://api.forecast.io/forecast/' + data.trim() + '/' + latitude + ',' + longitude;
     console.log(forecastURL);
+    requestForecastData();
   });
 }
 
